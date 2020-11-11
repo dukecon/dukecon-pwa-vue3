32fcbf6efacec7d0b45d817356ed70224c7022eb
Stephanie Peters <stephaniep@topdesk.com>
2020-11-11 16:20:01 +0100

    begin some layouting

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-	<div id="nav">
+	<header class="header">
+		Logo
 		<router-link to="/">Home</router-link> |
 		<router-link to="/about">About</router-link>
-	</div>
-	<side-menu>
-		<filters />
-	</side-menu>
-	<router-view />
+	</header>
+	<main class="box">
+		<side-menu>
+			<Suspense>
+				<template #default>
+						<filters />
+				</template>
+				<template #fallback>...</template>
+			</Suspense>
+		</side-menu>
+		<Suspense>
+			<template #default>
+				<router-view />
+			</template>
+			<template #fallback>
+				<div class="is-loading">&nbsp;</div>
+			</template>
+		</Suspense>
+	</main>
+	<footer class="footer">Footer here</footer>
 </template>
 
 <script>
@@ -18,5 +34,19 @@ export default {
 		SideMenu,
 		Filters,
 	},
+	setup() {
+
+	}
 };
 </script>
+<style>
+#app {
+	height: 100vh;
+	display: grid;
+	grid-template-rows: 2em 1fr 2em;
+}
+.footer {
+	height: 2em;
+	padding: 0;
+}
+</style>
