08e10fc8d2a4f67283135352c2242fb7d5749237
Stephanie Peters <stephaniep@topdesk.com>
2020-11-30 17:33:54 +0100

    replace tabs with navbar (WIP)

--- src/components/header/NavMenu.vue
+++ src/components/header/NavMenu.vue
@@ -1,8 +1,38 @@
 <template>
-	<nav class="nav tabs">
-		<ul>
-			<li :class="{'is-active': $route.path==='/'}"><router-link to="/" class="has-text-link-light">Home</router-link></li>
-			<li :class="{'is-active': $route.path==='/about'}"><router-link to="/about" class="has-text-link-light">About</router-link></li>
-		</ul>
-	</nav>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+	<div class="navbar-brand">
+		<button
+			:class="{'is-active': isOpen }"
+			role="button"
+			class="navbar-burger button has-background-transparent is-borderless"
+			aria-label="menu"
+			aria-expanded="false"
+			@click="toggle()">
+			<span aria-hidden="true"></span>
+			<span aria-hidden="true"></span>
+			<span aria-hidden="true"></span>
+		</button>
+	</div>
+	<div :class="{'is-active': isOpen }" class="navbar-menu">
+		<router-link :class="{'is-active': $route.path==='/'}" to="/" class="navbar-item">Home</router-link>
+		<router-link :class="{'is-active': $route.path==='/about'}" to="/about" class="navbar-item">About</router-link>
+	</div>
+</nav>
 </template>
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+	setup() {
+		const isOpen = ref(false);
+		const toggle = () =>  {
+			isOpen.value = !isOpen.value
+		};
+
+		return {
+			isOpen,
+			toggle,
+		}
+	}
+});
+</script>
+
