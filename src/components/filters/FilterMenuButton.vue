aa0830fe2e6d00d5b23c7c6acb5752da120d4fd0
Stephanie Peters <stephaniep@topdesk.com>
2020-12-02 16:39:55 +0100

    fix dropdown-menu styles (mobile) add filter icon

---
+++ src/components/filters/FilterMenuButton.vue
@@ -0,0 +1,3 @@
+<template>
+	<img src="@/images/filter.png" alt="" />
+</template>
--- src/views/TalksOverview.vue
+++ src/views/TalksOverview.vue
@@ -3,8 +3,10 @@
 		<template #default>
 			<main class="main box">
 				<side-menu title="Show/Hide Filters">
-					<template v-slot:toggle-button-icon>☰</template>  <!-- pulled out of SideMenu.vue so we can add badges and things -->
-					<filters />
+					<template v-slot:toggle-button-icon>
+						<filter-menu-button />
+					</template>  <!-- pulled out of SideMenu.vue so we can add badges and things -->
+					<filter-menu :filters="filters"/>
 				</side-menu>
 				<div class="main-area talks-overview container">
 					<!-- includes tabs for day selection and the grid separated by time slots -->
@@ -25,22 +27,26 @@
 // @ is an alias to /src
 import { computed } from 'vue';
 import { useStore } from 'vuex';
-import SideMenu from '@/components/SideMenu.vue';
-import Filters from '@/components/Filters.vue';
+import SideMenu from '@/components/SideMenu.vue'; // need to add extension because TS compiler does not know it (BUG?)
+import FilterMenuButton from '@/components/filters/FilterMenuButton.vue';
+import FilterMenu from '@/components/filters/FilterMenu.vue';
 import TalksGrid from '@/components/overview/TalksGrid.vue';
 
 export default {
 	components: {
 		SideMenu,
-		Filters,
+		FilterMenuButton,
+		FilterMenu,
 		TalksGrid,
 	},
 	setup() {
 		const store = useStore();
 		const events = computed(() => store.getters.filteredBySearchstring);
+		const filters = computed(() => store.state.filters);
 
 		return {
 			events,
+			filters,
 		}
 	}
 }
