db84838579a3085788b99eb32c435d57dcd93813
Stephanie Peters <stephaniep@topdesk.com>
2020-12-15 17:56:31 +0100

    nav-menu closes on click outside

--- src/components/header/NavMenu.vue
+++ src/components/header/NavMenu.vue
@@ -1,27 +1,30 @@
 <template>
-<nav class="navbar" role="navigation" aria-label="main navigation" @keyup.esc="close()">
-	<div class="navbar-brand">
-		<button
-			:class="{'is-active': isOpen }"
-			role="button"
-			class="navbar-burger button has-background-transparent is-borderless"
-			aria-label="menu"
-			:aria-expanded="isOpen"
-			@click="toggle()">
-			<span aria-hidden="true"></span>
-			<span aria-hidden="true"></span>
-			<span aria-hidden="true"></span>
-		</button>
-	</div>
-	<div :class="{'is-active': isOpen }" class="navbar-menu">
-		<router-link :class="{'is-active': $route.path==='/'}" to="/" class="navbar-item">{{$t('talks')}}</router-link>
-		<router-link :class="{'is-active': $route.path==='/timetable'}" to="/timetable" class="navbar-item">{{$t('timetable')}}</router-link>
-		<router-link :class="{'is-active': $route.path==='/speaker'}" to="/speaker" class="navbar-item">{{$t('speaker')}}</router-link>
-		<router-link :class="{'is-active': $route.path==='/about'}" to="/about" class="navbar-item">{{$t('feedback')}}</router-link>
-		<external-link v-if="homepageUrl" :href="homepageUrl" className="navbar-item" >{{homepageTitle}}</external-link>
-		<language-select class="navbar-item" />
-	</div>
-</nav>
+    <teleport to="body">
+		<div :class="{'is-active': isOpen }" class="nav-menu-veil" aria-hidden="true" @click="close()" @keyup.esc="close()">&nbsp;</div>
+	</teleport>
+	<nav class="navbar" role="navigation" aria-label="main navigation" @keyup.esc="close()">
+		<div class="navbar-brand">
+			<button
+				:class="{'is-active': isOpen }"
+				role="button"
+				class="navbar-burger button has-background-transparent is-borderless"
+				aria-label="menu"
+				:aria-expanded="isOpen"
+				@click="toggle()">
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+			</button>
+		</div>
+		<div :class="{'is-active': isOpen }" class="navbar-menu">
+			<router-link :class="{'is-active': $route.path==='/'}" to="/" class="navbar-item">{{$t('talks')}}</router-link>
+			<router-link :class="{'is-active': $route.path==='/timetable'}" to="/timetable" class="navbar-item">{{$t('timetable')}}</router-link>
+			<router-link :class="{'is-active': $route.path==='/speaker'}" to="/speaker" class="navbar-item">{{$t('speaker')}}</router-link>
+			<router-link :class="{'is-active': $route.path==='/about'}" to="/about" class="navbar-item">{{$t('feedback')}}</router-link>
+			<external-link v-if="homepageUrl" :href="homepageUrl" className="navbar-item" >{{homepageTitle}}</external-link>
+			<language-select class="navbar-item" />
+		</div>
+	</nav>
 </template>
 <script lang="ts">
 import { defineComponent, ref, computed } from 'vue';
